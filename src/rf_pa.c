e710f6669af428cd998e7d4613e4a8efaceae9af
Telekatz <telekatz@gmx.de>
2026-09-10 17:36:59 +0200

    Debug PA overlay fixed

--- src/rf_pa.c
+++ src/rf_pa.c
@@ -482,7 +482,7 @@ void debug_pa_loop(float p, float i, float d, float error, uint16_t vdet_mv, uin
                         default:
                             continue;
                     }
-                    canvas_char_write(1, ROW_OFFSET + row, buffer, strlen(buffer));
+                    canvas_char_write(1, ROW_OFFSET + row, buffer, strlen(buffer),0);
                 } break;
                 case 1: {
                     switch (row) {
@@ -493,7 +493,7 @@ void debug_pa_loop(float p, float i, float d, float error, uint16_t vdet_mv, uin
                         default:
                             continue;
                     }
-                    canvas_char_write(COLUMN_SIZE - strlen(buffer) - 1, ROW_OFFSET + row, buffer, strlen(buffer));
+                    canvas_char_write(COLUMN_SIZE - strlen(buffer) - 1, ROW_OFFSET + row, buffer, strlen(buffer),0);
 
 
                 } break;
